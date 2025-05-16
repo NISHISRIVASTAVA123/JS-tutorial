d6453cd6a2eb3c9b32751b56cb3e52f12500563a
NISHISRIVASTAVA123 <156221754+NISHISRIVASTAVA123@users.noreply.github.com>
2025-05-16 06:07:07 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) //we are using node.js not browser
+console.log(3+
+
+3)// code readability must be high 
+// null - standalone type 
+//symbol - for uniqueness
+// object
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof 3) // number
+console.log(typeof "3") // string
+console.log(typeof true) // boolean
+console.log(typeof Symbol()) // symbol
+// console.log(typeof BigInt(3)) // bigint
