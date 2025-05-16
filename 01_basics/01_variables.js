e321e9fb8b0195e85f7781ab61c65530c1c7e1f3
NISHISRIVASTAVA123 <156221754+NISHISRIVASTAVA123@users.noreply.github.com>
2025-05-16 06:38:31 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId="1234";
+let accountEmail="ns@gmail.com";
+var accountPassword="1234";
+accountcity="delhi";
+let accountstate;
+console.table([accountId, accountEmail, accountPassword, accountcity,accountstate]);
